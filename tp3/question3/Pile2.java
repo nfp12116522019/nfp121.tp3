f5f9f221d148db0aeb912897b11e9a97a3e9b746
Marc Khoury <marc.khoury@isae.edu.lb>
2019-04-14 06:16:02 -0700

    Add files via upload

--- tp3/question3/Pile2.java
+++ tp3/question3/Pile2.java
@@ -9,33 +9,146 @@ public class Pile2<T> implements PileI<T>{
     /** par d√©l√©gation : utilisation de la class Stack */
     private Stack<T> stk;
     /** la capacit√© de la pile */
-    private int capacit√©;
+    private int capacite;
 
     /** Cr√©ation d'une pile.
      * @param taille la "taille maximale" de la pile, doit √™tre > 0
      */
     public Pile2(int taille){
-        // √† compl√©ter
+        if (taille < 0) {
+            taille = PileI.CAPACITE_PAR_DEFAUT;
+        }
+        this.capacite = taille;
     }
 
+    /**
+     * CrÈe une nouvelle instance de {@see question2.Pile2} avec une "taille maximale" par dÈfaut ‡ {@value question3.PileI#CAPACITE_PAR_DEFAUT}.
+     */
     public Pile2(){
-        // √† compl√©ter
+        this(PileI.CAPACITE_PAR_DEFAUT);
+    }
+    
+    /**
+     * Retourne la taille actuelle de la pile.
+     * 
+     * @return La taille de la pile. 
+     */
+    public int taille() {
+        return stk.size();
+    }
+    
+    /**
+     * Retourne la capacitÈ maximale de la pile.
+     * 
+     * @return La capacitÈ de la pile.
+     */
+    public int capacite() {
+        return capacite;
     }
 
-    public void empiler(T o) throws PilePleineException{
-        // √† compl√©ter
+    /**
+     * Ajoute un objet en haut de la pile.
+     * 
+     * @param obj Objet ‡ ajouter.
+     */
+    public void empiler(T obj) throws PilePleineException{
+        if (obj == null) {
+            throw new IllegalArgumentException();
+        }
+        if (estPleine()) {
+            throw new PilePleineException();
+        }
+
+        stk.push(obj);
     }
 
+    /**
+     * Retire et retourne l'objet au sommet de la pile.
+     * 
+     * @return L'objet au sommet de la pile.
+     */
     public T depiler() throws PileVideException{
-        // √† compl√©ter
+        if (estVide()) {
+            throw new PileVideException();
+        }
+        return stk.pop();
     }
 
+    /**
+     * Retourne l'objet au sommet de la pile sans le retirer.
+     * 
+     * @return L'objet au sommet de la pile.
+     */
     public T sommet() throws PileVideException{
-        // √† compl√©ter
+        if (estVide()) {
+            throw new PileVideException();
+        }
+        return stk.peek();
     }
-
-    // recopier ici toutes les autres m√©thodes
-    // qui ne sont pas modifi√©es en fonction
-    // du type des √©l√©ments de la pile
-
-} // Pile2
+    
+    /**
+     * DÈfinit si la pile est pleine.
+     * 
+     * @return Un boolÈen indiquant si la pile est pleine.
+     */
+    public boolean estPleine() {
+        return stk.size() == capacite;
+    }
+    
+    /**
+     * DÈfinit si la pile est vide (ne contient aucun ÈlÈment).
+     * 
+     * @return Un boolÈen indiquant si la pile est vide.
+     */
+    public boolean estVide() {
+        return stk.isEmpty();
+    }
+        
+    /**
+     * Compare l'ÈgalitÈ de deux piles.
+     * 
+     * @return Un boolÈen indiquant l'ÈgalitÈ ou non de des deux piles.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        boolean equals = false;
+        if (obj instanceof Pile2) {
+            Pile2<T> pile = (Pile2<T>)obj;
+            
+            equals = capacite() == pile.capacite() && taille() == pile.taille();
+            for (int i = stk.size() - 1; i >= 0; i--) {
+                
+                T a = stk.elementAt(i), b = pile.stk.elementAt(i);
+                if (a != null) {
+                    equals &= a.equals(b);
+                } else {
+                    equals = false;
+                }                   
+                if (!equals) {
+                    
+                }
+            }
+        }
+        return equals;
+    }
+    
+    
+    public int hashCode() {
+        return toString().hashCode();
+    }
+    
+    
+    public String toString() {
+        StringBuffer sb = new StringBuffer("[");
+        for (int i = stk.size() - 1; i >= 0; i--) {
+            Object item = stk.elementAt(i);
+            if (item != null) {
+                sb.append(item.toString());
+                if (i > 0) {
+                    sb.append(", ");
+                }
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }}
--- tp3/question3/UneUtilisation.java
+++ tp3/question3/UneUtilisation.java
@@ -3,12 +3,10 @@ package question3;
 import question1.PolygoneRegulier;
 
 public class UneUtilisation {
+ public static void main(String[] args) throws Exception {
+        PileI<PolygoneRegulier> p1 = new Pile2<PolygoneRegulier>();
+        PileI<PileI<PolygoneRegulier>> p2 = new Pile2<PileI<PolygoneRegulier>>();
 
-    public static void main(String[] args) throws Exception {
-        // déclarer p1
-        // déclarer p2
-
-        // p1 est ici une pile de polygones réguliers PolygoneRegulier.java
         p1.empiler(new PolygoneRegulier(4, 100));
         p1.empiler(new PolygoneRegulier(5, 100));
 
@@ -18,14 +16,11 @@ public class UneUtilisation {
         System.out.println(" la pile p2 = " + p2);
 
         try {
-            // p1.empiler(new PolygoneRegulier(5,100)); // désormais une erreur de
-            // compilation
-            // ....
-            // String s = (String)p1.depiler(); // désormais une erreur de
-            // compilation
+            p1.empiler(new PolygoneRegulier(5,100));
+            // Erreur � la compilation ici
+            // String s = (String)p1.depiler();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
 }
